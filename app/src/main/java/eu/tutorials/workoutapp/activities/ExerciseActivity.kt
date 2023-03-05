524c22d96b8ceabdd0db71462ed484167725be11
hasandalkilic1 <hsn.dlklc222@gmail.com>
2023-03-05 16:35:26 +0300

    added mediplayer and textToSpeach LESSON 5

--- app/src/main/java/eu/tutorials/workoutapp/activities/ExerciseActivity.kt
+++ app/src/main/java/eu/tutorials/workoutapp/activities/ExerciseActivity.kt
@@ -1,16 +1,23 @@
 package eu.tutorials.workoutapp.activities
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import eu.tutorials.workoutapp.R
 import eu.tutorials.workoutapp.models.ExerciseModel
 import eu.tutorials.workoutapp.utils.Constants
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var restTimer:CountDownTimer?=null
     private var restProgress=0
@@ -21,11 +28,17 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList:ArrayList<ExerciseModel>? = null
     private var currentExercisePosition=-1
 
+    private var tts:TextToSpeech?=null
+
+    private var player:MediaPlayer?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
 
         exerciseList=Constants.defaultExerciseList()
+
+        tts= TextToSpeech(this,this,)
 
         setupActionBar()
 
@@ -49,16 +62,31 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setupRestView(){
 
+        try {
+            val soundURI= Uri.parse("android.resource://eu.tutorials.workoutapp/"
+                    +R.raw.app_src_main_res_raw_press_start)
+            player=MediaPlayer.create(applicationContext,soundURI)
+            player!!.isLooping=false
+            player!!.start()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
         flRestView.visibility=View.VISIBLE
         tvTitle.visibility=View.VISIBLE
         tvExerciseName.visibility=View.INVISIBLE
         flExerciseView.visibility=View.INVISIBLE
         ivImage.visibility=View.INVISIBLE
+        tvUpcomingLabel.visibility=View.VISIBLE
+        tvUpcomingExerciseName.visibility=View.VISIBLE
+
+        tvUpcomingExerciseName.text=exerciseList!![currentExercisePosition+1].getName()
 
         if (restTimer!=null){
             restTimer!!.cancel()
             restProgress=0
         }
+
         setRestProgressBar()
     }
 
@@ -69,11 +97,15 @@ class ExerciseActivity : AppCompatActivity() {
         tvExerciseName.visibility=View.VISIBLE
         flExerciseView.visibility=View.VISIBLE
         ivImage.visibility=View.VISIBLE
+        tvUpcomingLabel.visibility=View.INVISIBLE
+        tvUpcomingExerciseName.visibility=View.INVISIBLE
 
         if (exerciseTimer!=null){
             exerciseTimer!!.cancel()
             exerciseProgress=0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
 
         ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
         tvExerciseName.text=exerciseList!![currentExercisePosition].getName()
@@ -135,5 +167,31 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer!!.cancel()
             restProgress=0
         }
+
+        if (tts !=null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        if(player!=null){
+            player!!.stop()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status==TextToSpeech.SUCCESS){
+            val result=tts!!.setLanguage(Locale.ENGLISH)
+
+            if(result==TextToSpeech.LANG_MISSING_DATA || result==TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("tts","The language specified is not supported")
+            }
+        }
+        else{
+            Log.e("tts","Initialization Failed")
+        }
+    }
+
+    private fun speakOut(text:String){
+        tts!!.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
     }
 }
--- app/src/main/java/eu/tutorials/workoutapp/activities/MainActivity.kt
+++ app/src/main/java/eu/tutorials/workoutapp/activities/MainActivity.kt
@@ -3,17 +3,17 @@ package eu.tutorials.workoutapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import eu.tutorials.workoutapp.databinding.ActivityMainBinding
+import eu.tutorials.workoutapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // A method for accessing xml items
-    private var binding:ActivityMainBinding?=null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        setContentView(R.layout.activity_main)
 
         flStart.setOnClickListener {
             val intent = Intent(this, ExerciseActivity::class.java)
@@ -21,8 +21,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        binding=null
-    }
 }
